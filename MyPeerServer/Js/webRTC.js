4a932e2a987c8d5aa788edc6b82632d0a72c817c
Matthew <climbandrave@gmail.com>
2015-11-11 21:05:57 +1300

    Initial checkin nodejs / webTRC

---
+++ MyPeerServer/Js/webRTC.js
@@ -0,0 +1,112 @@
+﻿var MyWebRTC = function (destinationID, serverCallback, onOpen) {
+    
+    if (!serverCallback) {
+        console.log('No server callback function found');
+    }
+    
+    var pc_constraints = { optional: [{ DtlsSrtpKeyAgreement: true }, { RtpDataChannels: true }] };
+    
+    var servers = {
+        'iceServers': [{ url: 'stun:stun01.sipphone.com' },
+                        { url: 'stun:stun.ekiga.net' },
+                        { url: 'stun:stun.fwdnet.net' },
+                        { url: 'stun:stun.ideasip.com' },
+                        { url: 'stun:stun.iptel.org' },
+                        { url: 'stun:stun.rixtelecom.se' },
+                        { url: 'stun:stun.schlund.de' },
+                        { url: 'stun:stun.l.google.com:19302' },
+                        { url: 'stun:stun1.l.google.com:19302' },
+                        { url: 'stun:stun2.l.google.com:19302' },
+                        { url: 'stun:stun3.l.google.com:19302' },
+                        { url: 'stun:stun4.l.google.com:19302' },
+                        { url: 'stun:stunserver.org' },
+                        { url: 'stun:stun.softjoys.com' },
+                        { url: 'stun:stun.voiparound.com' },
+                        { url: 'stun:stun.voipbuster.com' },
+                        { url: 'stun:stun.voipstunt.com' },
+                        { url: 'stun:stun.voxgratia.org' },
+                        { url: 'stun:stun.xten.com' }]
+    };
+    
+    var _localConnection = new RTCPeerConnection(servers);
+    var _dataChannel     = _localConnection.createDataChannel('myDataChannel', { reliable: true });
+    
+    _dataChannel.onopen = function (event) {
+        trace('Data channel opened for client ' + destinationID);
+        
+        onOpen(event);
+    };
+    
+    _dataChannel.onerror = function (e) {
+        console.error('Channel error :', e)
+    };
+    
+    _dataChannel.onmessage = function (evt) {
+        console.log('Got message : ', evt.data);
+    }
+    
+    function onDataChannel(event) {
+        receiveChannel = event.channel;
+        receiveChannel.onmessage = function (event) {
+            alert(event.data);
+        };
+    }    ;
+    
+    trace("Created local peer connection object localPeerConnection");
+    _localConnection.ondatachannel = onDataChannel;
+    _localConnection.onicecandidate = _handleIceCandidate;
+    
+    
+    function _startOffer() {
+        _localConnection.createOffer(_gotLocalDescription);
+    }
+    
+    function _acceptOffer(message) {
+        _localConnection.setRemoteDescription(new RTCSessionDescription(message))
+    }
+    
+    function _sendAnswer(message) {
+        _acceptOffer(message);
+        _localConnection.createAnswer(_gotLocalDescription, null, pc_constraints);
+    }
+    
+    function _setCandidate(message) {
+        var candidate = new RTCIceCandidate({
+            sdpMLineIndex: message.label,
+            candidate: message.candidate
+        });
+        
+        _localConnection.addIceCandidate(candidate);
+    }
+    
+    function _handleIceCandidate(event) {
+        if (event.candidate) {
+            serverCallback({
+                type: 'candidate',
+                destinationID : destinationID,
+                data: event.candidate
+            });
+        }
+    }
+    
+    function _gotLocalDescription(desc) {
+        _localConnection.setLocalDescription(desc);
+        trace("Offer from localPeerConnection: \n" + desc.sdp);
+        desc.destinationID = destinationID;
+        serverCallback(desc);
+    }
+    
+    function trace(text) {
+        console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+    }
+    
+    return {
+        localConnection : _localConnection,
+        dataChannel     : _dataChannel,
+        startOffer      : _startOffer,
+        setCandidate    : _setCandidate,
+        sendAnswer      : _sendAnswer,
+        acceptOffer     : _acceptOffer
+    };
+}
+
---
+++ MyPeerServer/server.js
@@ -0,0 +1,55 @@
+﻿var http      = require('http')
+var express   = require('express');
+var app       = module.exports.app = express();
+var server    = http.createServer(app);
+var io        = require('socket.io').listen(server);
+var port      = process.env.port || 1337;
+var clientIDs = []
+
+function log(logMessage){
+    console.log(new Date().toLocaleTimeString() + ': ' + logMessage)
+}
+
+io.sockets.on('connection', function (client){
+ 
+    log('Socket connection. Client id assigned : ' + client.id);
+        
+    client.emit('connected', { clientID : client.id });
+    
+    client.on('iAmHere', function (message) {
+        log('ClientID : ' + message.clientID);
+        log('I am here request');
+        
+        client.broadcast.emit(message.type, message);
+    });
+
+    client.on('offer', function (message) {
+        log('ClientID : ' + message.clientID);
+        log('Offer SDP data : ' + message.sdp);
+
+        client.to(message.destinationID).emit(message.type, message);
+    });
+
+    client.on('answer', function (message) {
+        log('ClientID : ' + message.clientID);
+        log('Answer SDP data : ' + message.sdp);
+
+        client.to(message.destinationID).emit(message.type, message);
+    })
+
+    client.on('candidate', function (message) {
+        log('ClientID : ' + message.clientID);
+        log('ICE candidate : ' + message.data.candidate);
+        
+        client.to(message.destinationID).emit('candidate', message);
+    });
+})
+
+app.use(express.static(__dirname + '/Public'))
+app.use(express.static(__dirname + '/Js'))
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/Html/Default.html')
+})
+
+console.log('listening on *:' + port);
+server.listen(port);
